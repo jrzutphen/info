7ffadc11b39f5955925fab6ce769139500ec1fe4
Luka Leer <luka.leer@gmail.com>
2024-10-06 16:56:02 +0200

    🔐 Switch to Insiders version of Material for MkDocs
    Signed-off-by: Luka Leer <luka.leer@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -3,10 +3,10 @@
 {
   packages = with pkgs; [
     (python3.withPackages (
-      ps: with ps; [ mkdocs-material ]
-      ++ mkdocs-material.optional-dependencies.recommended
-      ++ mkdocs-material.optional-dependencies.git
-      ++ mkdocs-material.optional-dependencies.imaging
+      ps: with ps; [ mkdocs-material-insiders ]
+      ++ mkdocs-material-insiders.optional-dependencies.recommended
+      ++ mkdocs-material-insiders.optional-dependencies.git
+      ++ mkdocs-material-insiders.optional-dependencies.imaging
     ))
   ];
 }
